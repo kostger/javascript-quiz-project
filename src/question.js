05b75ba3fb0e182bffe251683eb85606635f8f6f
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-09-23 15:18:35 +0200

    Day 1

--- src/question.js
+++ src/question.js
@@ -2,6 +2,15 @@ class Question {
     // YOUR CODE HERE:
     //
     // 1. constructor (text, choices, answer, difficulty)
+    constructor(text,choices,answer,difficulty){
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
     // 2. shuffleChoices()
+    shuffleChoices(){
+        this.choices.sort((a,b)=>0.5 - Math.random());
+    }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,33 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions,timeLimit,timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
     // 2. getQuestion()
-    
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
     // 3. moveToNextQuestion()
-
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
     // 4. shuffleQuestions()
-
+    shuffleQuestions(){
+        this.questions.sort((a,b)=>0.5 - Math.random());
+    }
     // 5. checkAnswer(answer)
+    checkAnswer(answer){
+        if(this.questions[this.currentQuestionIndex].answer === answer)this.correctAnswers++;
+    }
 
     // 6. hasEnded()
+    hasEnded(){
+        return this.currentQuestionIndex < this.questions.length? false : true;
+    }
 }
