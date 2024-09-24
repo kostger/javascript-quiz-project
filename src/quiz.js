61066ebdf160b9ca3ee3a84ffb1f403a433dccbd
Konstantinos Gerogiannis <kostger2@gmail.com>
2024-09-24 15:00:44 +0200

    Day 2

--- src/quiz.js
+++ src/quiz.js
@@ -31,4 +31,13 @@ class Quiz {
     hasEnded(){
         return this.currentQuestionIndex < this.questions.length? false : true;
     }
+
+    filterQuestionsByDifficulty(difficulty){
+        if(difficulty === 1 || difficulty === 2 || difficulty === 3)this.questions = this.questions.filter((question)=> question.difficulty === difficulty);
+    }
+
+    averageDifficulty(){
+        return this.questions.reduce((acc,curr)=> acc+curr.difficulty,0)/this.questions.length
+    }
+
 }
